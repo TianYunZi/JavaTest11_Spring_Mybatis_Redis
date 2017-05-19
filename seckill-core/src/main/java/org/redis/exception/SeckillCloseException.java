36d92c1015004567b136a941df67e394f578eeb4
zxysegh@gmail.com <20150130TUESDAy>
2017-05-19 22:45:35 +0800

    补充缺失的部分

---
+++ seckill-core/src/main/java/org/redis/exception/SeckillCloseException.java
@@ -0,0 +1,14 @@
+package org.redis.exception;
+
+/**
+ * Created by XJX on 2017/5/19.
+ */
+public class SeckillCloseException extends SeckillException {
+    public SeckillCloseException(String message) {
+        super(message);
+    }
+
+    public SeckillCloseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
